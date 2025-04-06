d3aa60e057a8bd9184faa78b2d3bf8ec3784dcec
vertoker <vertogpro@gmail.com>
2025-04-06 10:08:46 +0500

    done with 02

--- catalog/02-cheapest-way.cpp
+++ catalog/02-cheapest-way.cpp
@@ -4,10 +4,13 @@
 	Сложность - Средняя
 	Тэги - dynamic programming 2D
 
-	Сложность (время)  - ЗАМЕНИТЬ_НА_COMPLEXITY_TIME
-	Сложность (память) - ЗАМЕНИТЬ_НА_COMPLEXITY_MEMORY
+	Сложность (время)  - O(N*M)
+	Сложность (память) - O(N*M)
 
-	ЗАМЕНИТЬ_НА_КРАТКОЕ_ОПИСАНИЕ_РЕШЕНИЯ
+	Классическая задача на динамическое программирование.
+	Для оптимизации сложности по памяти, я использовал тот же массив
+	и сразу после ввода значения сразу после вычислял вес клетки
+	относительно предыдущих
 
 	made by vertoker
 */
@@ -15,8 +18,33 @@
 #include <iostream>
 #include <cstdint>
 
-int main()
+int main() 
 {
-	
+	uint16_t N, M;
+	std::cin >> N >> M;
+
+	// max = 20 * 20 * 100 = 40000
+	uint16_t weights[N][M];
+
+	for	(size_t i = 0; i < N; ++i)
+	{
+		for	(size_t j = 0; j < M; ++j)
+		{
+			// Ввод
+			std::cin >> weights[i][j];
+
+			// Расчёт текущей клетки на основе предыдущей
+			uint16_t buffer = 65535;
+			if (i != 0 && j != 0)
+				weights[i][j] += std::min( weights[i-1][j], weights[i][j-1] );
+			else if (i != 0)
+				weights[i][j] += weights[i-1][j];
+			else if (j != 0)
+				weights[i][j] += weights[i][j-1];
+		}
+	}
+
+	std::cout << weights[N-1][M-1];
+
 	return 0;
 }
