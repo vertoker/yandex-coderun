6c80a4a95ece209edba820b3ffa8dd9b27beb1a6
vertoker <vertogpro@gmail.com>
2025-04-06 09:37:15 +0500

    start 02

---
+++ catalog/02-cheapest-way.cpp
@@ -0,0 +1,20 @@
+/*
+	Имя - cheapest-way
+	URL - https://coderun.yandex.ru/problem/cheapest-way
+	Сложность - Средняя
+	Тэги - dynamic programming 2D
+
+	Сложность (время)  - ЗАМЕНИТЬ_НА_COMPLEXITY_TIME
+	Сложность (память) - ЗАМЕНИТЬ_НА_COMPLEXITY_MEMORY
+
+	ЗАМЕНИТЬ_НА_КРАТКОЕ_ОПИСАНИЕ_РЕШЕНИЯ
+
+	made by vertoker
+*/
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
