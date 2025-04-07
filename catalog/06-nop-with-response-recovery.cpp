d68142de802d6720f31a4cf8230efa1bca0c19ea
vertoker <vertogpro@gmail.com>
2025-04-07 07:30:39 +0500

    failed 05 and add 06

---
+++ catalog/06-nop-with-response-recovery.cpp
@@ -0,0 +1,21 @@
+/*
+	Имя - НОП с восстановлением ответа
+	URL - https://coderun.yandex.ru/problem/nop-with-response-recovery
+	Сложность - Средняя
+	Тэги - dynamic_programming_2D
+
+	Сложность (время)  = ЗАМЕНИТЬ_НА_COMPLEXITY_TIME
+	Сложность (память) = ЗАМЕНИТЬ_НА_COMPLEXITY_MEMORY
+
+	ЗАМЕНИТЬ_НА_КРАТКОЕ_ОПИСАНИЕ_РЕШЕНИЯ
+
+	made by vertoker
+*/
+
+#include <iostream>
+#include <cstdint>
+
+int main()
+{
+	return 0;
+}
